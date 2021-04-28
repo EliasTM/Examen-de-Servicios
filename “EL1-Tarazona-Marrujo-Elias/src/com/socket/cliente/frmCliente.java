a7b79b6310c3d7f0cf193f2b4463c203397d15c0
xt3599 <xt3599@xt3599.com>
2021-04-27 19:15:36 -0500

    fix

--- “EL1-Tarazona-Marrujo-Elias/src/com/socket/cliente/frmCliente.java
+++ “EL1-Tarazona-Marrujo-Elias/src/com/socket/cliente/frmCliente.java
@@ -1,4 +1,5 @@
 package com.socket.cliente;
+
 import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -13,88 +14,86 @@ import java.util.List;
 import java.awt.event.ActionEvent;
 import javax.swing.ImageIcon;
 
-public class frmCliente extends JFrame implements ActionListener{
-	
-	private JPanel contentPane;
-	private JButton btnEnviar;
-	private JButton btnCierre;
+public class frmCliente extends JFrame implements ActionListener {
 
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					frmCliente frame = new frmCliente();
-					frame.setVisible(true);
-				}
-				catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
+  private JPanel contentPane;
+  private JButton btnEnviar;
+  private JButton btnCierre;
 
-	/**
-	 * Create the frame.
-	 */
-	public frmCliente() {
-		setTitle("Cliente");
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		//setDefaultCloseOperation(0);
-		setBounds(100, 100, 558, 231);
-		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		setContentPane(contentPane);
-		contentPane.setLayout(null);
-		
-		btnEnviar = new JButton("");
-		btnEnviar.setIcon(new ImageIcon(frmCliente.class.getResource("/img/mensajeCliente.png")));
-		btnEnviar.addActionListener(this);
-		btnEnviar.setBounds(124, 73, 89, 73);
-		contentPane.add(btnEnviar);
-		
-		btnCierre = new JButton("");
-		btnCierre.setIcon(new ImageIcon(frmCliente.class.getResource("/img/cerrarCliente.png")));
-		btnCierre.addActionListener(this);
-		btnCierre.setBounds(376, 73, 89, 73);
-		contentPane.add(btnCierre);
-	}
-	public void actionPerformed(final ActionEvent arg0) {
-		if (arg0.getSource() == btnCierre) {
-			actionPerformedBtnCierre(arg0);
-		}
-		if (arg0.getSource() == btnEnviar) {
-			actionPerformedBtnEnviar(arg0);
-		}
-	}
-	protected void actionPerformedBtnEnviar(final ActionEvent arg0) {
-		try {
-			Socket enviar=new Socket("localhost",1026);
-			List<Supervisor> lista=new ArrayList<Supervisor>();
-			lista.add(new Supervisor(1, 25000, true));
-			lista.add(new Supervisor(2, 35000, false));
-			lista.add(new Supervisor(3, 45000, true));
-			lista.add(new Supervisor(4, 55000, false));
-			ObjectOutputStream stream=new ObjectOutputStream(enviar.getOutputStream());
-			stream.writeObject(lista);
-			enviar.close();
+  /**
+   * Launch the application.
+   */
+  public static void main(String[] args) {
+    EventQueue.invokeLater(new Runnable() {
+      public void run() {
+        try {
+          frmCliente frame = new frmCliente();
+          frame.setVisible(true);
+        } catch (Exception e) {
+          e.printStackTrace();
+        }
+      }
+    });
+  }
 
-			
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
+  /**
+   * Create the frame.
+   */
+  public frmCliente() {
+    setTitle("Cliente");
+    setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    //setDefaultCloseOperation(0);
+    setBounds(100, 100, 558, 231);
+    contentPane = new JPanel();
+    contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+    setContentPane(contentPane);
+    contentPane.setLayout(null);
 
-		
-	}
-	
-	protected void actionPerformedBtnCierre(final ActionEvent arg0) {
-		System.exit(0);
-	}
+    btnEnviar = new JButton("");
+    btnEnviar.setIcon(new ImageIcon(frmCliente.class.getResource("/img/mensajeCliente.png")));
+    btnEnviar.addActionListener(this);
+    btnEnviar.setBounds(124, 73, 89, 73);
+    contentPane.add(btnEnviar);
 
-	
+    btnCierre = new JButton("");
+    btnCierre.setIcon(new ImageIcon(frmCliente.class.getResource("/img/cerrarCliente.png")));
+    btnCierre.addActionListener(this);
+    btnCierre.setBounds(376, 73, 89, 73);
+    contentPane.add(btnCierre);
+  }
 
-	
-		
+  public void actionPerformed(final ActionEvent arg0) {
+    if (arg0.getSource() == btnCierre) {
+      actionPerformedBtnCierre(arg0);
+    }
+    if (arg0.getSource() == btnEnviar) {
+      actionPerformedBtnEnviar(arg0);
+    }
+  }
+
+  protected void actionPerformedBtnEnviar(final ActionEvent arg0) {
+    try {
+      Socket enviar = new Socket("localhost", 1026);
+      List<Supervisor> lista = new ArrayList<Supervisor>();
+      lista.add(new Supervisor(1, 25000, true));
+      lista.add(new Supervisor(2, 35000, false));
+      lista.add(new Supervisor(3, 45000, true));
+      lista.add(new Supervisor(4, 55000, false));
+      ObjectOutputStream stream = new ObjectOutputStream(enviar.getOutputStream());
+      stream.writeObject(lista);
+      enviar.close();
+
+
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+
+
+  }
+
+  protected void actionPerformedBtnCierre(final ActionEvent arg0) {
+    System.exit(0);
+  }
+
+
 }
--- “EL1-Tarazona-Marrujo-Elias/src/com/socket/entidad/Supervisor.java
+++ “EL1-Tarazona-Marrujo-Elias/src/com/socket/entidad/Supervisor.java
@@ -3,93 +3,58 @@ package com.socket.entidad;
 import java.io.Serializable;
 
 public class Supervisor implements Serializable {
-	
-	private int codigo;
-	private double sueldo;
-	private boolean condicion;
-	
-	public Supervisor(int codigo, double sueldo, boolean condicion) {
-		super();
-		this.codigo = codigo;
-		this.sueldo = sueldo;
-		this.condicion = condicion;
-	}
-	
-	
 
-	 
-	public int getCodigo() {
-		return codigo;
-	}
+  private int codigo;
+  private double sueldo;
+  private boolean condicion;
 
+  public Supervisor(int codigo, double sueldo, boolean condicion) {
+    super();
+    this.codigo = codigo;
+    this.sueldo = sueldo;
+    this.condicion = condicion;
+  }
 
+  public int getCodigo() {
+    return codigo;
+  }
 
+  public void setCodigo(int codigo) {
+    this.codigo = codigo;
+  }
 
-	public void setCodigo(int codigo) {
-		this.codigo = codigo;
-	}
+  public double getSueldo() {
+    return sueldo;
+  }
 
+  public void setSueldo(double sueldo) {
+    this.sueldo = sueldo;
+  }
 
+  public boolean isCondicion() {
+    return condicion;
+  }
 
+  public void setCondicion(boolean condicion) {
+    this.condicion = condicion;
+  }
 
-	public double getSueldo() {
-		return sueldo;
-	}
+  public String validacion() {
+    if (condicion) {
+      return "Estable";
+    } else {
+      return "Por campaña";
+    }
 
+  }
 
+  public Supervisor() {
+    super();
+  }
 
-
-	public void setSueldo(double sueldo) {
-		this.sueldo = sueldo;
-	}
-
-
-
-
-	public boolean isCondicion() {
-		return condicion;
-	}
-
-
-
-
-	public void setCondicion(boolean condicion) {
-		this.condicion = condicion;
-	}
-
-
-
-
-	public String validacion() {
-		if(condicion == true) {
-			return "Estable";
-		}else {
-			return "Por campaña";
-		}
-			
-	}
-
-
-	public Supervisor() {
-		super();
-	}
-
-
-
-
-	public Supervisor(boolean condicion) {
-		super();
-		this.condicion = condicion;
-	}
-
-	
-
-	
-	
-	
-	
-	
-	
-	
+  public Supervisor(boolean condicion) {
+    super();
+    this.condicion = condicion;
+  }
 
 }
